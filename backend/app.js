4fe46aacf11ce3bbaa3d5805b7551314858054dd
Harsh-kumar-2024 <hkakadiya3102@gmail.com>
2023-02-17 11:43:04 +0530

    Added Database Connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+require('./db/conn');
 app.get('/', (req, res)=> {
     res.send("Hello World");
 })
---
+++ backend/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://manthan:manthan123@cluster0.etx0kun.mongodb.net/mernstack?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB Connected");
+}).catch((err) => {
+    console.log(err);
+});
