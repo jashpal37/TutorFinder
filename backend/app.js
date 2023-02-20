d7147aa72683b4a85348fb238498414eb9cdc280
Harsh-kumar-2024 <hkakadiya3102@gmail.com>
2023-02-20 19:31:14 +0530

    changed backend port

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res)=> {
     res.send("Hello World");
 })
 
-app.listen(3000,()=>{
-    console.log("Listening on port 3000...");
+app.listen(8000,()=>{
+    console.log("Listening on port 8000...");
 });
