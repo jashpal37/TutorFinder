e06d3c2b730b4d3833f3108cbea2d1033c803cc7
jashpal37 <jashpalsinhparmar422@gmail.com>
2023-02-24 10:36:29 +0530

    card

---
+++ frontend/src/components/TutorCard.jsx
@@ -0,0 +1,48 @@
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Typography,
+  CardContent,
+} from "@mui/material";
+import { MailOutlined, ChromeReaderMode } from "@mui/icons-material";
+import { red } from "@mui/material/colors";
+import React from "react";
+
+function TutorCard() {
+  return (
+    <>
+      <Card sx={{ maxWidth: 350, maxHeight: 350,border:1,margin:5 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          title="Rahul"
+          sx={{border:1}}
+        />
+        <CardContent>
+          <Typography>Code</Typography>
+          <Typography letterSpacing={2}>
+            <MailOutlined />
+            rahul12@gmail.com
+          </Typography>
+          <Typography letterSpacing={2}>
+            <ChromeReaderMode />
+            Subject Name
+          </Typography>
+          <Typography>Description:</Typography>
+          <Typography paragraph>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            dolore itaque, eius libero atque voluptatibus minus. Impedit aut
+            consectetur libero hic ratione recusandae distinctio sed ut omnis.
+            Nam, delectus ipsum?
+          </Typography>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
+
+export default TutorCard;
