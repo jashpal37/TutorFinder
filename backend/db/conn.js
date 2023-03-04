b40b2038ec60150a95c97fb7fa77cbc9366b6e41
Harsh-kumar-2024 <hkakadiya3102@gmail.com>
2023-03-04 12:08:11 +0530

    change conn url

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 mongoose
   .connect(
-    "mongodb://manthan:manthan123@ac-iswaaki-shard-00-00.etx0kun.mongodb.net:27017,ac-iswaaki-shard-00-01.etx0kun.mongodb.net:27017,ac-iswaaki-shard-00-02.etx0kun.mongodb.net:27017/?ssl=true&replicaSet=atlas-ag1a0d-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "mongodb+srv://manthan:<password>@cluster0.etx0kun.mongodb.net/mernstack?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
